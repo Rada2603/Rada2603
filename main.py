eb2680223ffb6f0886d3dbbf2488b8d5c7660e92
Rada2603 <145929352+Rada2603@users.noreply.github.com>
2023-09-24 21:04:45 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+from utils import check_option
+from models.agents import Agents
+from models.clients import Clients
+from utils import check_option_a
+
+
+def main():
+    print("\nWelcome to agency")
+    Agent = Agents(r"c:\Users\Lenovo\Documents\Rada\Learning_1\agencija\data\agents.txt")
+    Agent.initialise_users()
+    Client = Clients(r"C:\Users\Lenovo\Documents\Rada\Learning_1\agencija\data\agents.txt")
+    Client.initialise_users()
+    while True:
+        print("____please login___")
+        print("a. I am agent")
+        print("b. I am client")
+        option = input("select an  option")
+        print(f"option:{option}")
+        if not check_option(option):
+            continue
+        if option == "a":
+            Name = input("name")
+            Password = input("password")
+            Agent.check_agent(Name, Password)
+            while True:
+                print("Select an option")
+                print("1.Logout")
+                print("2.Creating  an engagement")
+                print("3.Creating a package")
+                print("4.Deleting an engagement")
+                option_a = input("Select an option")
+                print(f"option_a:{option_a}")
+                if  check_option_a(option_a):
+                    continue
+        if option == "b":
+            Name = input("name")
+            Password = input("password")
+            Client.check_client(Name, Password)
+
+
+if __name__ == "__main__":
+    main()
