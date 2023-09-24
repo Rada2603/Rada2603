001dcfb3bda8a171f424b4923b40e89402e200e7
Rada2603 <145929352+Rada2603@users.noreply.github.com>
2023-09-24 21:11:06 +0200

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,21 @@
+def check_option(option: str) -> bool:
+    if option == "a" or option == "b":
+        return True
+    else:
+        print("\n>>>Wrong choice\n")
+        return False
+    
+    
+def check_option_a(option_a: str) -> bool:
+    try:
+        if int(option_a) < 1 or int(option_a) > 4:
+            print("\n>>>Wrong choice\n")
+            return False
+        else:
+            return True
+    except Exception:
+        print(">>>>>wrong\n")
+        return False
+
+
+
